bd48927215a01efba62bbe5487a632049a83e4d0
Kristine <86106513+kristineng@users.noreply.github.com>
2025-01-21 17:24:42 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,24 +3,24 @@
 
 ## Write a short comment describing this function
 cacheMatrix <- function(matrix = matrix()) {
-    invMat <- NULL  ## hold the inverse matrix when calculated
+    inv <- NULL  ## hold the inverse matrix when calculated
     assignMatrix <- function(newMatrix) {  ## set a new matrix in the parent environment
         matrix <<- newMatrix
-        invMat <<- NULL  ## because the matrix has changed
+        inv <<- NULL  ## because the matrix has changed
     }
     retrieveMatrix <- function() matrix  ## retrieve the current matrix
-    cacheInverse <- function(inverse) invMat <<- inverse  ## set the cached inverse matrix
-    getInverse <- function() invMat  ## get the cached inverse matrix
+    cacheInverse <- function(inverse) inv <<- inverse  ## set the cached inverse matrix
+    getInverse <- function() inv  ## get the cached inverse matrix
     list(set = assignMatrix, get = retrieveMatrix, cacheInverse = cacheInverse, getInverse = getInverse)  ## Return a list of functions
 }
 solveMatrix <- function(matrixObj, ...) {
-    invMat <- matrixObj$getInverse() 
-    if(!is.null(invMat)) {
+    inv <- matrixObj$getInverse() 
+    if(!is.null(inv)) {
         message("cached result")
-        return(invMat)
+        return(inv)
     }
     mat <- matrixObj$get()  ## get the matrix to be inverted
-    invMat <- solve(mat, ...) 
-    matrixObj$cacheInverse(invMat) 
-    invMat 
+    inv <- solve(mat, ...) 
+    matrixObj$cacheInverse(inv) 
+    inv 
 }
