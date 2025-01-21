54ebee6e3b6688327926ecd3581b791dd8aff092
Kristine <86106513+kristineng@users.noreply.github.com>
2025-01-21 17:18:53 +0800

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,26 +2,25 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x=matrix()) {    ## define the element 
-    inv <- NULL                              ## will hold value of of matrix inverse
-    set <- function(y) {                     ##
-        x <<- y
-        inv <<- NULL
+cacheMatrix <- function(matrix = matrix()) {
+    invMat <- NULL  ## hold the inverse matrix when calculated
+    assignMatrix <- function(newMatrix) {  ## set a new matrix in the parent environment
+        matrix <<- newMatrix
+        invMat <<- NULL  ## because the matrix has changed
     }
-    get <- function() x                      ## returns the value of the matrix argument
-    setInverse <- function(inverse) inv<<- inverse ## assigns the value of inv in parent environment
-    getInverse <- function() inv    ##get the value of inv where called
-    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+    retrieveMatrix <- function() matrix  ## retrieve the current matrix
+    cacheInverse <- function(inverse) invMat <<- inverse  ## set the cached inverse matrix
+    getInverse <- function() invMat  ## get the cached inverse matrix
+    list(set = assignMatrix, get = retrieveMatrix, cacheInverse = cacheInverse, getInverse = getInverse)  ## Return a list of functions
 }
-cacheSolve <- function(x, ...) {
-    inv <- x$getInverse()
-    if(!is.null(inv)) {        ##check if the inv is already cached
-        message('geting cached data')##if true, display message
-        return(inv)
+solveMatrix <- function(matrixObj, ...) {
+    invMat <- matrixObj$getInverse() 
+    if(!is.null(invMat)) {
+        message("cached result")
+        return(invMat)
     }
-    data <- x$get()        ##get the matrix to be inverted
-    inv <- solve(data, ...)
-    x$setInverse(inv)
-    inv
+    mat <- matrixObj$get()  ## get the matrix to be inverted
+    invMat <- solve(mat, ...) 
+    matrixObj$cacheInverse(invMat) 
+    invMat 
 }
